444ddc082789b9c130bbdf25477231f32d8b6cc9
yambo92 <1810962886@qq.com>
2020-04-15 23:30:02 +0800

    'lot'

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,102 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-//http request 1
-exports.randomNumber = functions.https.onRequest((request, response) => {
-    const number = Math.round(Math.random() * 100);
-    response.send(number.toString());
+admin.initializeApp();
+//auth trigger (new user signup)
+exports.newUserSignup = functions.auth.user().onCreate((user) => {
+    // console.log('user created', user.email, user.uid);
+    //Function returned undefined, expected Promise or value
+    return admin.firestore().collection('users').doc(user.uid).set({
+        email: user.email,
+        upvotedOn: []
+    });
+    
 })
 
-//http request 2
-exports.toTheDojo = functions.https.onRequest((request, response) => {
-    response.redirect('https://yambo-firestore-tut.web.app/')
+//auth trigger (user deleted)
+exports.userDeleted = functions.auth.user().onDelete((user) => {
+    // console.log('user deleted', user.email, user.uid);
+    //Function returned undefined, expected Promise or value
+    const doc = admin.firestore().collection('users').doc(user.uid);
+    return doc.delete();
+    
 });
 
-//http callable function
-exports.sayHello = functions.https.onCall((data, context) => {
-    const name = data.name;
-    return `hello , ${name}`
+//http callable function (adding a request)
+exports.addRequest = functions.https.onCall((data, context) => {
+    if(!context.auth){
+        throw new functions.https.HttpsError(
+            'unauthenticated', 
+            'only authentaicated user can add requests.'
+        );
+    } else if(data.ct.length > 30){
+        throw new functions.https.HttpsError(
+            'invalid-argument',
+            'request must be no more than 30 characters long.'
+        )
+    } else {
+         admin.firestore().collection('requests').add({
+            text: data.ct,
+            upvotes: 0
+        }).then((result) => {
+            return result
+            
+        }).catch(error => console.log(error.message))
+    }
 })
+
+// upvote callable function
+exports.upvote = functions.https.onCall(( data, context) => {
+    //check auth
+    if(!context.auth){
+        throw new functions.https.HttpsError(
+            'unauthenticated', 
+            'only authentaicated user can add vote.'
+        );
+    }
+
+    // get refs for user doc & request doc
+    const user = admin.firestore().collection('users').doc(context.auth.uid);
+    const request = admin.firestore().collection('requests').doc(data.id);
+
+    return user.get().then(doc => {
+        //check user hasn't already upvoted the request
+        if(doc.data().upvotedOn.includes(data.id)){
+            throw new functions.https.HttpsError(
+                "failed-precondition", 
+                'you can only upvote something once'
+            )
+        }
+        //update user array
+        return user.update({
+            upvotedOn: [...doc.data().upvotedOn, data.id]
+        })
+        .then(() => {
+            //update votes on the request
+            return request.update({
+                //把当前这个request里的upvotes字段的值增1；
+                upvotes: admin.firestore.FieldValue.increment(1)
+            })
+        })
+    })
+})
+
+
+
+
+// //http request 1
+// exports.randomNumber = functions.https.onRequest((request, response) => {
+//     const number = Math.round(Math.random() * 100);
+//     response.send(number.toString());
+// })
+
+// //http request 2
+// exports.toTheDojo = functions.https.onRequest((request, response) => {
+//     response.redirect('https://yambo-firestore-tut.web.app/')
+// });
+
+// //http callable function
+// exports.sayHello = functions.https.onCall((data, context) => {
+//     const name = data.name;
+//     return `hello , ${name}`
+// })
--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 const requestModal = document.querySelector('.new-request');
 const requestLink = document.querySelector('.add-request');
-
+const requestForm = document.querySelector('.new-request form');
 //open request modal
 requestLink.addEventListener('click', () => {
     requestModal.classList.add('open')
@@ -13,13 +13,42 @@ requestModal.addEventListener('click', (e) => {
     }
 })
 
+// add a new request
+requestForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const addRequest = functions.httpsCallable('addRequest');
+    addRequest({
+        ct: requestForm.request.value
+    }).then(() => {
+        requestForm.reset();
+        requestModal.classList.remove('open');
+        requestForm.querySelector('.error').textContent = '';
+    }).catch(error => {
+        requestForm.querySelector('.error').textContent = error.message
+    })
+})
+
+
+//notification
+const notification = document.querySelector('.notification')
+
+const showNotification = (message) => {
+    notification.textContent = message;
+    notification.classList.add('active');
+    setTimeout(() => {
+        notification.classList.remove('active')
+        notification.textContent = ''
+    }, 2000);
+}
+
 // say hello function call
 const button = document.querySelector('.call')
-button.addEventListener('click', () => {
-    //get function reference
-    const sayHello =functions.httpsCallable('sayHello');
-    sayHello({ name: 'shaun' }).then(result => {
-        console.log(result.data);
-    });
+// button.addEventListener('click', () => {
+//     //get function reference
+//     const sayHello =functions.httpsCallable('sayHello');
+//     sayHello({ name: 'shaun' }).then(result => {
+//         console.log(result.data);
+//     });
 
-})
+// })
